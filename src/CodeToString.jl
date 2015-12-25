02b22f58ffe4c4a249ea8884a60fe98a1a92ef51
Peter <xianrenb@gmail.com>
2015-12-25 16:52:17 +0800

    changed CodeToString.jl

--- src/CodeToString.jl
+++ src/CodeToString.jl
@@ -1,5 +1,48 @@
 module CodeToString
 
-# package code goes here
+export code_llvm_to_bytestring, @code_llvm_to_bytestring, code_native_to_bytestring, @code_native_to_bytestring, code_warntype_to_bytestring, @code_warntype_to_bytestring
 
-end # module
+macro stdout_to_bytestring(ex)
+    quote
+        old_stdout = STDOUT
+        (rd, wr) = redirect_stdout()
+        $(esc(ex))
+        close(wr)
+        data = readavailable(rd)
+        close(rd)
+        redirect_stdout(old_stdout)
+        data
+    end
+end
+
+function code_llvm_to_bytestring(f, types)
+    @stdout_to_bytestring code_llvm(f, types)
+end
+
+macro code_llvm_to_bytestring(ex)
+    quote
+        @stdout_to_bytestring @code_llvm $(esc(ex))
+    end
+end
+
+function code_native_to_bytestring(f, types)
+    @stdout_to_bytestring code_native(f, types)
+end
+
+macro code_native_to_bytestring(ex)
+    quote
+        @stdout_to_bytestring @code_native $(esc(ex))
+    end
+end
+
+function code_warntype_to_bytestring(f, types)
+    @stdout_to_bytestring code_warntype(f, types)
+end
+
+macro code_warntype_to_bytestring(ex)
+    quote
+        @stdout_to_bytestring @code_warntype $(esc(ex))
+    end
+end
+
+end #module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,22 @@
 using CodeToString
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+try
+    f(a, b) = a + b
+    code_llvm_to_bytestring(f, (Int64, Int64))
+    @code_llvm_to_bytestring f(1,2)
+    @test typeof(code_llvm_to_bytestring(f, (Int64, Int64))) == Array{UInt8,1}
+    @test typeof(@code_llvm_to_bytestring f(1,2)) == Array{UInt8,1}
+    code_native_to_bytestring(f, (Int64, Int64))
+    @code_native_to_bytestring f(1,2)
+    @test typeof(code_native_to_bytestring(f, (Int64, Int64))) == Array{UInt8,1}
+    @test typeof(@code_native_to_bytestring f(1,2)) == Array{UInt8,1}
+    code_warntype_to_bytestring(f, (Int64, Int64))
+    @code_warntype_to_bytestring f(1,2)
+    @test typeof(code_warntype_to_bytestring(f, (Int64, Int64))) == Array{UInt8,1}
+    @test typeof(@code_warntype_to_bytestring f(1,2)) == Array{UInt8,1}
+catch
+    @test false
+end
+
+@test true
